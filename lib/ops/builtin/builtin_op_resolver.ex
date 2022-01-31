0158d226fe5e994ffe03c27dff3e9790179a213b
Cocoa <r.xu.1@research.gla.ac.uk>
2022-01-31 05:57:38 +0000

    docs: add inline docs for TFLite.Ops.Builtin.BuiltinResolver

--- lib/ops/builtin/builtin_op_resolver.ex
+++ lib/ops/builtin/builtin_op_resolver.ex
@@ -2,11 +2,23 @@ defmodule TFLite.Ops.Builtin.BuiltinResolver do
   @type nif_resource_ok :: {:ok, reference()}
   @type nif_error :: {:error, String.t()}
 
+  @doc """
+  New built-in op resolver.
+
+  This resolver provides a list of TfLite delegates that could be
+  applied by TfLite interpreter by default.
+  """
   @spec new() :: nif_resource_ok() | nif_error()
   def new() do
     TFLite.Nif.ops_builtin_builtinResolver_new()
   end
 
+  @doc """
+  New built-in op resolver.
+
+  This resolver provides a list of TfLite delegates that could be
+  applied by TfLite interpreter by default.
+  """
   @spec new!() :: reference()
   def new!() do
     {:ok, resolver} = new()
