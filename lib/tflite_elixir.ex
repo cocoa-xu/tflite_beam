bc2bfea6bf556bf02dbae5d16e78d2824d414506
Cocoa <r.xu.1@research.gla.ac.uk>
2022-01-31 05:58:54 +0000

    docs: add inline docs for TFLite

--- lib/tflite_elixir.ex
+++ lib/tflite_elixir.ex
@@ -1,4 +1,9 @@
 defmodule TFLite do
+  @doc """
+  Prints a dump of what tensors and what nodes are in the interpreter.
+
+  Note that this function directly prints to stdout
+  """
   @spec printInterpreterState(reference()) :: nil
   def printInterpreterState(interpreter) do
     TFLite.Nif.tflite_printInterpreterState(interpreter)
