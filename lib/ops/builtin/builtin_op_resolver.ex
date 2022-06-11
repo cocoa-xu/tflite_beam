6a319496c9cfc90722f24ca97f8297b32318a359
Cocoa <r.xu.1@research.gla.ac.uk>
2022-06-11 21:56:16 +0100

    use TFLiteElixir.Errorize

--- lib/ops/builtin/builtin_op_resolver.ex
+++ lib/ops/builtin/builtin_op_resolver.ex
@@ -1,4 +1,6 @@
 defmodule TFLiteElixir.Ops.Builtin.BuiltinResolver do
+  import TFLiteElixir.Errorize
+
   @type nif_resource_ok :: {:ok, reference()}
   @type nif_error :: {:error, String.t()}
 
@@ -13,15 +15,5 @@ defmodule TFLiteElixir.Ops.Builtin.BuiltinResolver do
     TFLiteElixir.Nif.ops_builtin_builtinResolver_new()
   end
 
-  @doc """
-  New built-in op resolver.
-
-  This resolver provides a list of TfLite delegates that could be
-  applied by TfLite interpreter by default.
-  """
-  @spec new!() :: reference()
-  def new!() do
-    {:ok, resolver} = new()
-    resolver
-  end
+  deferror(new())
 end
