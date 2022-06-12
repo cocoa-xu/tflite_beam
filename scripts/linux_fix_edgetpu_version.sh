2865ee3ee8c8efebd9c4e438353dfcb7a42511df
Cocoa <r.xu.1@research.gla.ac.uk>
2022-06-12 22:44:27 +0100

    fix Linux

--- scripts/linux_fix_edgetpu_version.sh
+++ scripts/linux_fix_edgetpu_version.sh
@@ -3,19 +3,17 @@
 case "$(uname -s)" in
   Linux*)
     LIBEDGETPU_DIR="$1"
-    LIBEDGETPU_WRONG_NAME="$(find "${LIBEDGETPU_DIR}" -size +1M -type f | head -n1)"
+    cd "${LIBEDGETPU_DIR}"
+    LIBEDGETPU_WRONG_NAME="$(find . -size +1M -type f | head -n1)"
     if [ -z "${LIBEDGETPU_WRONG_NAME}" ]; then
-      echo "cannot find libedgetpu.so.1 or libedgetpu.so in ${LIBEDGETPU_DIR}"
+      echo "cannot find libedgetpu.so.1.0 or libedgetpu.so.1 or libedgetpu.so in ${LIBEDGETPU_DIR}"
       exit 1
     fi
 
-    if [ ! -e "${LIBEDGETPU_DIR}/libedgetpu.so.1" ]; then
-      cp -a "${LIBEDGETPU_WRONG_NAME}" "${LIBEDGETPU_DIR}/libedgetpu.so.1" ;
-    fi
-    if [ ! -e "${LIBEDGETPU_DIR}/libedgetpu.so" ]; then
-      cd "${LIBEDGETPU_DIR}" ;
-      ln -s "${LIBEDGETPU_WRONG_NAME}" "${LIBEDGETPU_DIR}/libedgetpu.so" ;
-    fi
+    rm -f "${LIBEDGETPU_DIR}/libedgetpu.so.1"
+    ln -s "${LIBEDGETPU_WRONG_NAME}" "libedgetpu.so.1"
+    rm -f "${LIBEDGETPU_DIR}/libedgetpu.so"
+    ln -s "${LIBEDGETPU_WRONG_NAME}" "libedgetpu.so"
     ;;
   *)
     ;;
