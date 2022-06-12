26608f4562eff1b93e2236aed395567391a4f381
Cocoa <r.xu.1@research.gla.ac.uk>
2022-06-12 21:21:59 +0100

    fix Linux

--- scripts/linux_fix_edgetpu_version.sh
+++ scripts/linux_fix_edgetpu_version.sh
@@ -4,12 +4,12 @@ case "$(uname -s)" in
   Linux*)
     LIBEDGETPU_DIR="$1"
     LIBEDGETPU_WRONG_NAME="$(find "${LIBEDGETPU_DIR}" -size +1M -type f)"
-    if [ "${LIBEDGETPU_WRONG_NAME}" != "${LIBEDGETPU_DIR}/libedgetpu.so" ]; then
+    if [ "${LIBEDGETPU_WRONG_NAME}" != "${LIBEDGETPU_DIR}/libedgetpu.so.1" ]; then
       if [ -z "${LIBEDGETPU_WRONG_NAME}" ]; then
         echo "cannot find libedgetpu in ${LIBEDGETPU_DIR}"
         exit 1
       fi
-      mv "${LIBEDGETPU_WRONG_NAME}" "${LIBEDGETPU_DIR}/libedgetpu.so" ;
+      mv "${LIBEDGETPU_WRONG_NAME}" "${LIBEDGETPU_DIR}/libedgetpu.so.1" ;
       find "${LIBEDGETPU_DIR}" -size -1024c -exec rm -f '{}' \;
     fi
     ;;
