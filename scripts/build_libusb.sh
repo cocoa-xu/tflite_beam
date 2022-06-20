c82df2fc3b5c66fd875516874a09aa758c8440aa
Cocoa <r.xu.1@research.gla.ac.uk>
2022-06-20 21:01:14 +0100

    compile libusb

--- scripts/build_libusb.sh
+++ scripts/build_libusb.sh
@@ -6,11 +6,11 @@ cd "${TFLITE_ROOT_SRC}"
 mkdir -p "${DESTDIR}"
 
 CROSSCOMPILE=""
-if [ -n "${TARGET_ARCH}" ] && [ -n "${TARGET_OS}" ]; then
-  CROSSCOMPILE="--host=\"$(TARGET_ARCH)-$(TARGET_OS)\""
+if [ -n "${TARGET_ARCH}" ] && [ -n "${TARGET_OS}" ] && [ -n "${TARGET_ABI}" ]; then
+  CROSSCOMPILE="--host=${TARGET_ARCH}-${TARGET_OS}-${TARGET_ABI}"
 fi
 git submodule update --init 3rd_party/libusb
 cd 3rd_party/libusb
 ./autogen.sh
-./configure CC="$(CC)" "${CROSSCOMPILE}" --enable-shared --enable-udev=no --prefix=/
+./configure CC="${CC}" "${CROSSCOMPILE}" --enable-shared --enable-udev=no --prefix=/
 make DESTDIR="${DESTDIR}" install
