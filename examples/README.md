6ae2e946d8fbeb86a2c002e09af4f429ce84a0e7
Cocoa <r.xu.1@research.gla.ac.uk>
2022-06-20 22:33:44 +0100

    cleanup

--- examples/README.md
+++ examples/README.md
@@ -2,3 +2,6 @@
 
 ## Super Resolution with ESRGAN
 [![Run in Livebook](https://livebook.dev/badge/v1/gray.svg)](https://livebook.dev/run?url=https%3A%2F%2Fgithub.com%2Fcocoa-xu%2Ftflite_elixir%2Fblob%2Fmain%2Fexamples%2Fsuper_resolution.livemd)
+
+## Inference on TPU (Coral)
+[![Run in Livebook](https://livebook.dev/badge/v1/gray.svg)](https://livebook.dev/run?url=https%3A%2F%2Fgithub.com%2Fcocoa-xu%2Ftflite_elixir%2Fblob%2Fmain%2Fexamples%2Ftpu.livemd)
--- scripts/postbuild_fix_libedgetpu.sh
+++ scripts/postbuild_fix_libedgetpu.sh
@@ -6,17 +6,8 @@ case "$(uname -s)" in
     LIBEDGETPU_DIR="$(dirname "${NATIVE_BINDINGS_SO}")/libedgetpu"
     cd "${LIBEDGETPU_DIR}"
     patchelf --replace-needed libusb-1.0.so.0 \$ORIGIN/lib/libusb-1.0.so.0 libedgetpu.so.1.0
-    # patchelf --replace-needed libedgetpu.so.1.0 \$ORIGIN/libedgetpu/libedgetpu.so.1.0 "${NATIVE_BINDINGS_SO}"
     ;;
   Darwin*)
-    #    LIBEDGETPU_PATH="$(otool -L "${NATIVE_BINDINGS_SO}" | grep 'libedgetpu.1.dylib' | awk -F' \\(compatibility' '/,/{gsub(/[ \t]/, "", $1);print $1}')"
-    #    EXPECTED_PATH="@loader_path/libedgetpu/libedgetpu.1.dylib"
-    #    if [ -n "${LIBEDGETPU_PATH}" ]; then
-    #      if [ "${LIBEDGETPU_PATH}" != "${EXPECTED_PATH}" ]; then
-    #        install_name_tool -change "${LIBEDGETPU_PATH}" "${EXPECTED_PATH}" "${NATIVE_BINDINGS_SO}"
-    #      fi
-    #    fi
-
     LIBEDGETPU_DIR="$(dirname "${NATIVE_BINDINGS_SO}")/libedgetpu"
     cd "${LIBEDGETPU_DIR}"
     LIBEDGETPU_WRONG_NAME="$(find . -name '*edgetpu*' -size +100k -type f | head -n1)"
