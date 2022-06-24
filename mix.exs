59ce822dd0bd5f36081edc67f06033720b575548
Cocoa <r.xu.1@research.gla.ac.uk>
2022-06-24 23:59:20 +0100

    map `"k8"` to `"x86_64"`

--- mix.exs
+++ mix.exs
@@ -97,6 +97,12 @@ defmodule TfliteElixir.MixProject do
   defp has_precompiled_binaries(tflite_version=@tflite_version, _enable_coral_support="YES", edgetpu_libraries) do
     case edgetpu_libraries do
       lib when lib in ["k8", "x86_64", "aarch64", "armv7a", "riscv64"] ->
+        lib =
+          if lib == "k8" do
+            "x86_64"
+          else
+            lib
+          end
         filename = "tflite_elixir-linux-#{lib}-#{@version}"
         {true, "#{@github_url}/releases/download/#{@version}/#{filename}.tar.gz", filename}
       lib when lib in ["darwin_arm64", "darwin_x86_64"] ->
