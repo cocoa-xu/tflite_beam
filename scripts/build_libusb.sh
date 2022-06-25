ca757f460167441aa5506c98a0b76c91396534b3
Cocoa <r.xu.1@research.gla.ac.uk>
2022-06-25 04:19:04 +0100

    fix macos crosscompile

--- scripts/build_libusb.sh
+++ scripts/build_libusb.sh
@@ -5,14 +5,30 @@ DESTDIR="$2"
 cd "${TFLITE_ROOT_SRC}"
 mkdir -p "${DESTDIR}"
 
+git submodule update --init 3rd_party/libusb
+cd 3rd_party/libusb
 CROSSCOMPILE=""
 if [ -n "${TARGET_ARCH}" ] && [ -n "${TARGET_OS}" ] && [ -n "${TARGET_ABI}" ]; then
   CROSSCOMPILE="--host=${TARGET_ARCH}-${TARGET_OS}-${TARGET_ABI}"
+  case "${TARGET_OS}" in
+    apple*)
+      ./autogen.sh CC="${CC}" CFLAGS="-arch ${TARGET_ARCH}" LDFLAGS="-arch ${TARGET_ARCH}" "${CROSSCOMPILE}" --enable-shared --disable-udev --prefix=/
+      ./configure CC="${CC}" CFLAGS="-arch ${TARGET_ARCH}" LDFLAGS="-arch ${TARGET_ARCH}" "${CROSSCOMPILE}" --enable-shared --disable-udev --prefix=/
+    ;;
+    linux*)
+      ./autogen.sh CC="${CC}" "${CROSSCOMPILE}" --enable-shared --disable-udev --prefix=/
+      ./configure CC="${CC}" "${CROSSCOMPILE}" --enable-shared --disable-udev --prefix=/
+    ;;
+    *)
+      echo "unsupported system: ${TARGET_OS}" ;
+      exit 1 ;
+    ;;
+  esac
+else
+  ./autogen.sh CC="${CC}" --enable-shared --disable-udev --prefix=/
+  ./configure CC="${CC}" --enable-shared --disable-udev --prefix=/
 fi
-git submodule update --init 3rd_party/libusb
-cd 3rd_party/libusb
-./autogen.sh
-./configure CC="${CC}" "${CROSSCOMPILE}" --enable-shared --disable-udev --prefix=/
+
 make DESTDIR="${DESTDIR}" install
 
 LIBUSB_SO="libusb-1.0.0.dylib"
