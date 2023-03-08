157063948a9430eafbd898b4ff9468dbdd19ff50
Cocoa <r.xu.1@research.gla.ac.uk>
2023-03-08 17:08:12 +0000

    fix armv7l toolchain

--- cc_toolchain/armv7l-linux-gnueabihf.cmake
+++ cc_toolchain/armv7l-linux-gnueabihf.cmake
@@ -4,8 +4,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR armv7l)
 
-set(CMAKE_C_COMPILER "/usr/bin/arm-linux-gnueabihf-gcc-9")
-set(CMAKE_CXX_COMPILER "/usr/bin/arm-linux-gnueabihf-g++-9")
+set(CMAKE_C_COMPILER "/usr/bin/arm-linux-gnueabihf-gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/arm-linux-gnueabihf-g++")
 
 set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf)
 
