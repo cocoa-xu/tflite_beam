bf3638955764dd0b9bee1cebaf83138ebee57a4f
cocoa-xu <i@uwucocoa.moe>
2023-03-08 13:46:49 +0000

    macos: fix libusb rpath

---
+++ scripts/macos_libusb_rpath.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+case "$(uname -s)" in
+  Darwin*)
+    TFLITE_ELIXIR_SO_FILE="$1"
+    LIBUSB_IN_TFLITE_ELIXIR_SO="$(otool -L "${TFLITE_ELIXIR_SO_FILE}" | grep libusb | awk -F' \\(compatibility' '/,/{gsub(/[ \t]/, "", $1);print $1}')"
+    LIBUSB="@rpath/libusb-1.0.0.dylib"
+    if [ "${LIBUSB_IN_TFLITE_ELIXIR_SO}" != "${LIBUSB}" ]; then
+      install_name_tool -change "${LIBUSB_IN_TFLITE_ELIXIR_SO}" "${LIBUSB}" "${TFLITE_ELIXIR_SO_FILE}"
+    fi
+    ;;
+  *)
+    ;;
+esac
