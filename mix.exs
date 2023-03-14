71321513c862bc87b8bf4d8699f41d078772a1a2
Cocoa <i@uwucocoa.moe>
2023-03-14 05:47:46 +0000

    v0.1.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TfliteElixir.MixProject do
   require Logger
 
   @app :tflite_elixir
-  @version "0.1.3"
+  @version "0.1.4"
   @tflite_version "2.11.0"
   @prefer_precompiled "YES"
   @github_url "https://github.com/cocoa-xu/tflite_elixir"
