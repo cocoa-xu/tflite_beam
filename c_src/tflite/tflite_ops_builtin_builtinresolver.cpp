72a8e78f728ccd339c65ae59b2d82aa598baf1e6
Cocoa <i@uwucocoa.moe>
2023-03-15 17:48:59 +0000

    improvements to build steps

---
+++ c_src/tflite/tflite_ops_builtin_builtinresolver.cpp
@@ -0,0 +1,18 @@
+#include <erl_nif.h>
+#include "../nif_utils.hpp"
+#include "../erlang_nif_resource.hpp"
+#include "../helper.h"
+#include "tensorflow/lite/kernels/register.h"
+#include "tensorflow/lite/kernels/builtin_op_kernels.h"
+
+ERL_NIF_TERM ops_builtin_builtinResolver_new(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+    erlang_nif_res<tflite::ops::builtin::BuiltinOpResolver *> * res;
+    if (alloc_resource(&res)) {
+        res->val = new tflite::ops::builtin::BuiltinOpResolver();
+        ERL_NIF_TERM ret = enif_make_resource(env, res);
+        enif_release_resource(res);
+        return erlang::nif::ok(env, ret);
+    } else {
+        return erlang::nif::error(env, "cannot allocate memory for resource");
+    }
+}
--- c_src/tflite/tflite_ops_builtin_builtinresolver.h
+++ c_src/tflite/tflite_ops_builtin_builtinresolver.h
@@ -3,16 +3,8 @@
 
 #pragma once
 
-static ERL_NIF_TERM ops_builtin_builtinResolver_new(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-    erlang_nif_res<tflite::ops::builtin::BuiltinOpResolver *> * res;
-    if (alloc_resource(&res)) {
-        res->val = new tflite::ops::builtin::BuiltinOpResolver();
-        ERL_NIF_TERM ret = enif_make_resource(env, res);
-        enif_release_resource(res);
-        return erlang::nif::ok(env, ret);
-    } else {
-        return erlang::nif::error(env, "cannot allocate memory for resource");
-    }
-}
+#include <erl_nif.h>
+
+ERL_NIF_TERM ops_builtin_builtinResolver_new(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
 
 #endif // TFLITE_OPS_BUILTIN_BUILTINRESOLVER_BINDINGS_H
