dfbc0c090ec15672b49336b9df068697e7a86410
Cocoa <i@uwucocoa.moe>
2023-03-20 21:30:15 +0000

    simplified test code

--- test/tflitetensor_test.exs
+++ test/tflitetensor_test.exs
@@ -1,20 +1,12 @@
 defmodule TFLiteElixir.TFLiteTensor.Test do
   use ExUnit.Case
 
-  alias TFLiteElixir.InterpreterBuilder
-  alias TFLiteElixir.Ops.Builtin.BuiltinResolver
-  alias TFLiteElixir.FlatBufferModel
   alias TFLiteElixir.Interpreter
   alias TFLiteElixir.TFLiteTensor
 
   test "type/1" do
     filename = Path.join([__DIR__, "test_data", "mobilenet_v2_1.0_224_inat_bird_quant.tflite"])
-    model = FlatBufferModel.build_from_file(filename)
-    resolver = BuiltinResolver.new!()
-    builder = InterpreterBuilder.new!(model, resolver)
-    interpreter = Interpreter.new!()
-    :ok = InterpreterBuilder.build!(builder, interpreter)
-    :ok = Interpreter.allocate_tensors(interpreter)
+    interpreter = Interpreter.new!(filename)
 
     t = Interpreter.tensor(interpreter, 0)
     assert {:u, 8} == TFLiteTensor.type(t)
@@ -27,12 +19,7 @@ defmodule TFLiteElixir.TFLiteTensor.Test do
 
   test "dims/1" do
     filename = Path.join([__DIR__, "test_data", "mobilenet_v2_1.0_224_inat_bird_quant.tflite"])
-    model = FlatBufferModel.build_from_file(filename)
-    resolver = BuiltinResolver.new!()
-    builder = InterpreterBuilder.new!(model, resolver)
-    interpreter = Interpreter.new!()
-    :ok = InterpreterBuilder.build!(builder, interpreter)
-    :ok = Interpreter.allocate_tensors(interpreter)
+    interpreter = Interpreter.new!(filename)
 
     t = Interpreter.tensor(interpreter, 0)
     assert [1, 224, 224, 3] == TFLiteTensor.dims(t)
@@ -45,12 +32,7 @@ defmodule TFLiteElixir.TFLiteTensor.Test do
 
   test "quantization_params/1" do
     filename = Path.join([__DIR__, "test_data", "mobilenet_v2_1.0_224_inat_bird_quant.tflite"])
-    model = FlatBufferModel.build_from_file(filename)
-    resolver = BuiltinResolver.new!()
-    builder = InterpreterBuilder.new!(model, resolver)
-    interpreter = Interpreter.new!()
-    :ok = InterpreterBuilder.build!(builder, interpreter)
-    :ok = Interpreter.allocate_tensors(interpreter)
+    interpreter = Interpreter.new!(filename)
     t = Interpreter.tensor(interpreter, 0)
 
     assert %TFLiteElixir.TFLiteQuantizationParams{
@@ -73,12 +55,7 @@ defmodule TFLiteElixir.TFLiteTensor.Test do
 
   test "set_data/2" do
     filename = Path.join([__DIR__, "test_data", "mobilenet_v2_1.0_224_inat_bird_quant.tflite"])
-    model = FlatBufferModel.build_from_file(filename)
-    resolver = BuiltinResolver.new!()
-    builder = InterpreterBuilder.new!(model, resolver)
-    interpreter = Interpreter.new!()
-    :ok = InterpreterBuilder.build!(builder, interpreter)
-    :ok = Interpreter.allocate_tensors(interpreter)
+    interpreter = Interpreter.new!(filename)
     t = Interpreter.tensor(interpreter, 0)
 
     zeros = Nx.broadcast(Nx.tensor(0, type: :u8), {1, 224, 224, 3})
@@ -105,12 +82,7 @@ defmodule TFLiteElixir.TFLiteTensor.Test do
 
   test "to_binary/2" do
     filename = Path.join([__DIR__, "test_data", "mobilenet_v2_1.0_224_inat_bird_quant.tflite"])
-    model = FlatBufferModel.build_from_file(filename)
-    resolver = BuiltinResolver.new!()
-    builder = InterpreterBuilder.new!(model, resolver)
-    interpreter = Interpreter.new!()
-    :ok = InterpreterBuilder.build!(builder, interpreter)
-    :ok = Interpreter.allocate_tensors(interpreter)
+    interpreter = Interpreter.new!(filename)
     t = Interpreter.tensor(interpreter, 0)
 
     ones = Nx.broadcast(Nx.tensor(1, type: :u8), {1, 224, 224, 3})
@@ -128,11 +100,7 @@ defmodule TFLiteElixir.TFLiteTensor.Test do
 
   test "to_nx/1" do
     filename = Path.join([__DIR__, "test_data", "mobilenet_v2_1.0_224_inat_bird_quant.tflite"])
-    model = FlatBufferModel.build_from_file(filename)
-    resolver = BuiltinResolver.new!()
-    builder = InterpreterBuilder.new!(model, resolver)
-    interpreter = Interpreter.new!()
-    :ok = InterpreterBuilder.build!(builder, interpreter)
+    interpreter = Interpreter.new!(filename)
     :ok = Interpreter.allocate_tensors(interpreter)
 
     t = Interpreter.tensor(interpreter, 0)
@@ -143,14 +111,9 @@ defmodule TFLiteElixir.TFLiteTensor.Test do
 
   test "to_nx/2" do
     filename = Path.join([__DIR__, "test_data", "mobilenet_v2_1.0_224_inat_bird_quant.tflite"])
-    model = FlatBufferModel.build_from_file(filename)
-    resolver = BuiltinResolver.new!()
-    builder = InterpreterBuilder.new!(model, resolver)
-    interpreter = Interpreter.new!()
-    :ok = InterpreterBuilder.build!(builder, interpreter)
-    :ok = Interpreter.allocate_tensors(interpreter)
-
+    interpreter = Interpreter.new!(filename)
     t = Interpreter.tensor(interpreter, 0)
+
     %Nx.Tensor{} = nx_tensor = TFLiteTensor.to_nx(t, backend: Nx.BinaryBackend)
     assert nx_tensor.shape == {1, 224, 224, 3}
     assert Nx.all_close(nx_tensor, 0)
@@ -163,14 +126,9 @@ defmodule TFLiteElixir.TFLiteTensor.Test do
 
   test "to_nx/2 with backend: nil" do
     filename = Path.join([__DIR__, "test_data", "mobilenet_v2_1.0_224_inat_bird_quant.tflite"])
-    model = FlatBufferModel.build_from_file(filename)
-    resolver = BuiltinResolver.new!()
-    builder = InterpreterBuilder.new!(model, resolver)
-    interpreter = Interpreter.new!()
-    :ok = InterpreterBuilder.build!(builder, interpreter)
-    :ok = Interpreter.allocate_tensors(interpreter)
-
+    interpreter = Interpreter.new!(filename)
     t = Interpreter.tensor(interpreter, 0)
+
     %Nx.Tensor{} = nx_tensor = TFLiteTensor.to_nx(t, backend: nil)
     assert nx_tensor.shape == {1, 224, 224, 3}
     assert Nx.all_close(nx_tensor, 0)
@@ -178,13 +136,7 @@ defmodule TFLiteElixir.TFLiteTensor.Test do
 
   test "to_nx/2 with wrong backend" do
     filename = Path.join([__DIR__, "test_data", "mobilenet_v2_1.0_224_inat_bird_quant.tflite"])
-    model = FlatBufferModel.build_from_file(filename)
-    resolver = BuiltinResolver.new!()
-    builder = InterpreterBuilder.new!(model, resolver)
-    interpreter = Interpreter.new!()
-    :ok = InterpreterBuilder.build!(builder, interpreter)
-    :ok = Interpreter.allocate_tensors(interpreter)
-
+    interpreter = Interpreter.new!(filename)
     t = Interpreter.tensor(interpreter, 0)
 
     assert_raise RuntimeError,
