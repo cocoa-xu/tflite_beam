11e3f7c7cbd408ee7cde3bdf067ac884ff56ea2d
Cocoa <i@uwucocoa.moe>
2023-03-29 16:54:07 +0100

    added `tflite_beam_tensor_type` typespec

--- src/tflite_beam/tflite_beam_records.hrl
+++ src/tflite_beam/tflite_beam_records.hrl
@@ -1,2 +1,19 @@
 -record(tflite_beam_error_reporter, {ref}).
 -record(tflite_beam_flatbuffer_model, {initialized, minimum_runtime, ref}).
+-type tflite_beam_tensor_type() :: no_type
+                                | {f, 32}
+                                | {s, 32}
+                                | {u, 8}
+                                | {s, 64}
+                                | string
+                                | bool
+                                | {s, 16}
+                                | {c, 64}
+                                | {s, 8}
+                                | {f, 16}
+                                | {f, 64}
+                                | {c, 128}
+                                | {u, 64}
+                                | resource
+                                | variant
+                                | {u, 32}.
