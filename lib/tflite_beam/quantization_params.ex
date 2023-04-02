0a0b74f5cf69be8d0becf8d1ed7544a29b50acc1
Cocoa <i@uwucocoa.moe>
2023-04-02 17:16:30 +0100

    removed Elixir project

--- lib/tflite_beam/quantization_params.ex
+++
@@ -1,9 +0,0 @@
-defmodule TFLiteBEAM.TFLiteQuantizationParams do
-  @moduledoc """
-  Quantization parameters that corresponds to the table QuantizationParameters
-  in the [TFLite Model schema file].
-
-  [TFLite Model schema file]: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/schema/schema.fbs
-  """
-  defstruct [:scale, :zero_point, :quantized_dimension]
-end
